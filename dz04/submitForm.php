c574034098152d904f5a7e406d2ea3b05bac0335
branislav-cvetanovic-1112 <banecve@hotmail.com>
2016-03-25 20:45:10 +0100

    domaci 4 commit

---
+++ dz04/submitForm.php
@@ -0,0 +1,13 @@
+<?php
+$to = "banecve@yahoo.com";
+$name = $_POST['yourName'];
+$email = $_POST['yourEmail'];
+$subject = $_POST['yourSubject'] ;
+$message = $_POST['yourDescription'] ;
+$headers = "From: $email";
+$sent = mail($to, $name, $email, $subject, $message, $headers);
+if($sent)
+{print "Your mail was sent successfully"; }
+else
+{print "We encountered an error sending your mail"; }
+?>
