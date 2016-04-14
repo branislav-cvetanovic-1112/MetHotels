488c34f2ae4437024d5425a9671edc93274ae41b
branislav-cvetanovic-1112 <banecve@hotmail.com>
2016-04-14 17:56:02 +0200

    IT255-dz07-branislav-cvetanovic-1112

--- DZ7/get.php
+++ DZ7/get.php
@@ -3,14 +3,23 @@
 $ime = $_GET['ime'];
 $prezime = $_GET['prezime'];
 $datum = $_GET['datum'];
-
-if (isset($_GET['submit'])) {
-  echo "Podaci: " . $ime . " " . $prezime . " " . $datum;
-}
 $bday = new DateTime($datum);
 $today = new DateTime(date("Y/m/d"));
 $diff = $today->diff($bday);
+
+echo "Podaci: " . $ime . " " . $prezime . " " . $datum;
 echo nl2br("\r\n");
-printf('i mator je: ' . '%d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
+
+if ($type == "json") {
+  header("Content-type: application/json");
+  $resenje = array('%d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
+  echo json_encode($resenje);
+} else {
+  header("Content-type: text/xml");
+  $resenje = printf('i mator je: ' . '%d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
+  $xml = new SimpleXMLElement('<root/>');
+  array_walk_recursive($resenje, array($xml, 'addChild'));
+  print $xml->asXML();
+}
 ?>
 
--- DZ7/post.php
+++ DZ7/post.php
@@ -1,15 +1,24 @@
 <?php
 
+$type = $_POST['type'];
 $ime = $_POST['ime'];
 $prezime = $_POST['prezime'];
 $datum = $_POST['datum'];
-
-if (isset($_POST['submit'])) {
-  echo "Podaci: " . $ime . " " . $prezime . " " . $datum;
-}
+//echo "Podaci: " . $ime . " " . $prezime . " " . $datum;
 $bday = new DateTime($datum);
 $today = new DateTime(date("Y/m/d"));
 $diff = $today->diff($bday);
-echo nl2br("\r\n");
-printf('i mator je: ' . '%d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
+echo nl2br("\r\n" . 'i mator je: ');
+
+if ($type == "json") {
+  header("Content-type: application/json");
+  $resenje = array('%d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
+  echo json_encode($resenje);
+} else {
+  header("Content-type: text/xml");
+  $resenje = printf('i mator je: ' . '%d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
+  $xml = new SimpleXMLElement('<root/>');
+  array_walk_recursive($resenje, array($xml, 'addChild'));
+  print $xml->asXML();
+}
 ?>
